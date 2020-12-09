45b42ce98f2ba40823b965be20e6bb30e5b9539d
Tom W <tom.w@med.app>
2020-12-09 12:47:05 +1100

    Updating for downloads per day + colors

--- raspberryScraper.js
+++ raspberryScraper.js
@@ -11,14 +11,19 @@ const release = new Date("February 19, 2019 11:46:11");
 const current = new Date();
 
 function dateDiff() {
-	let difference = current - release;
+	const difference = current - release;
 	return Math.round(difference / (60 * 60 * 24 * 1000));
 }
 
+function perDay(string) {
+	const downloads = string;
+	return Math.round(parseInt(downloads.slice(0, -9).replace(/,/g, '')) / dateDiff());
+}
+
 rp(url)
-	.then(function(html) {
+	.then(function (html) {
 		//success!
-		console.log(chalk.hex("#e592faff").bold(" WOW! raspberryCandy has:"));
+		console.log(chalk.hex("#00feff").bold("WOW!") + chalk.hex("#e592faff").bold(" raspberryCandy has:"));
 		console.log(
 			chalk.hex("#00feff")(
 				figlet.textSync($("span.installs-text", html).text(), {
@@ -28,11 +33,12 @@ rp(url)
 				})
 			)
 		);
-		console.log(chalk.hex("#00feff").bold("It was released on: " + release));
+		console.log(chalk.hex("#00feff").bold("It was released on: " + chalk.hex("#e592faff").bold(release)));
 		console.log(
-			chalk.hex("#e592faff").bold("It has been released for: " + dateDiff() + " days!")
+			chalk.hex("#e592faff").bold("It has been released for: " + chalk.hex("#00feff").bold(dateDiff()) + " days!")
 		);
+		console.log(chalk.hex("#00feff").bold("That's " + chalk.hex("#e592faff").bold(perDay($("span.installs-text", html).text())) + " downloads per day!"));
 	})
-	.catch(function(err) {
+	.catch(function (err) {
 		//handle error
 	});
